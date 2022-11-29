14318c2ef4341b17f5b38259b0f27a5ab18685b2
LinaSeskuviene <ses.lina@gmail.com>
2022-11-29 20:38:43 +0200

    login finished

--- src/App.js
+++ src/App.js
@@ -5,16 +5,35 @@ import { LoginForm } from './LoginForm';
 
 
 function App() {
+  const userData = {
+    username: "admin",
+    password: "pass123"
+  }
+  const [username, setUsername] = useState({username: "", password: ""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+
+    if (details.username === userData.username && details.password === userData.password) {
+      setUsername({
+        username: details.username,
+        password: details.password
+      });      
+    } else {
+      setError("Incorrect username or password")
+    }
+    } 
+
   return (
     <div className="App">
-      <header className="App-header">
-      
-        <p>
-         Hello
-        </p>
-        <LoginForm />
-  
-      </header>
+      {(username.username !== "") ? (
+        <div>
+          <p>you in</p>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} /> 
+      )
+    }   
     </div>
   );
 }
--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -2,25 +2,26 @@ import React from 'react';
 import './css/loginForm.css';
 import { useState } from 'react';
 
-export function LoginForm({submitLogin, error}) {
-    const [loginData, setLoginData] = useState({username: "", password: ""})
+export function LoginForm({Login, error}) {
+    const [details, setDetails] = useState({username: "", password: ""})
     function handleSubmit(event) {
         event.preventDefault()
-        console.log(loginData)
+        Login(details)
     }
 
     return ( 
         <div className="loginContainer"> 
             <h1>Welcome to event management system</h1> 
+            {(error !== "") ? (<div>{error}</div>) : ""}
             <form className="loginForm" onSubmit={handleSubmit}>
                 <input className="loginInput" type="text" name="username" 
                 id="username" placeholder="username" 
-                onChange={event => setLoginData({...loginData, username: event.target.value})}
-                value={loginData.username} />
+                onChange={event => setDetails({...details, username: event.target.value})}
+                value={details.username} />
                 <input className="loginInput" type="password" name="password" 
                 id="password" placeholder="password" 
-                onChange={event => setLoginData({...loginData, password: event.target.value})}
-                value={loginData.password}/>  
+                onChange={event => setDetails({...details, password: event.target.value})}
+                value={details.password}/>  
                 <button type="submit" className="submitButton">Log in</button>
             </form>
         </div>
